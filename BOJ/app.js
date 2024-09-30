15a06054e5719e5ed4c559d21ac80d6ae4e41ecf
sol <nathf1005@naver.com>
2024-10-01 00:42:35 +0900

    init

---
+++ BOJ/app.js
@@ -0,0 +1,6 @@
+// let input = require('fs').readFileSync('./input.txt').toString()
+
+
+let input = require('fs').readFileSync('/dev/stdin').toString()
+input = input.split(' ')
+console.log('input', input)
